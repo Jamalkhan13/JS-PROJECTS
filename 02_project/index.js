12da952fd82481c8a97326fa8f101e1c5ed66392
Jamalkhan13 <jamalkhan7858.gmail.com>
2024-11-18 15:30:06 +0500

    Auto Text Effect Animation

---
+++ 02_project/index.js
@@ -0,0 +1,26 @@
+const containerElement = document.querySelector(".conatiner")
+
+const careers = ["Youtuber","Web Developer","Freelancer","Instructor"]
+
+let careerIndex = 0;
+let characterIndex = 0;
+function updateText (){
+  characterIndex++
+  containerElement.innerHTML = `<h1>i am ${careers[careerIndex].slice(0,1) === "I" ? "an" :"a"} ${careers[careerIndex].slice(0,characterIndex)} </h1>`;
+  // characterIndex++
+  if(characterIndex === careers[careerIndex].length){
+    careerIndex++;
+    characterIndex = 0
+
+  }
+  if(careerIndex === careers.length){
+    careerIndex = 0
+  }
+  setTimeout(()=>{
+    updateText()
+  },400)
+
+}
+updateText()
+
+
