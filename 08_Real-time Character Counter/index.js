cff42a63ab032c6784439be9296092b533eff975
Jamalkhan13 <jamalkhan7858@gmail.com>
2024-11-28 12:11:49 +0500

    Real-time character Counter

--- 08_Real-time Character Counter/index.js
+++ 08_Real-time Character Counter/index.js
@@ -12,6 +12,22 @@ updateCounter();
 
  function updateCounter(){
     totalCounterElement.innerHTML = textareaElement.value.length;
+    
 
+    // get the maxlength from html
     remainingCounterElement.innerText = textareaElement.getAttribute("maxlength") - textareaElement.value.length
+
+
+    // second method : set the attribute of maxlength in JS and then get that to display
+
+    // remainingCounterElement.innerText = textareaElement.setAttribute("maxlength","200")
+    // remainingCounterElement.innerText = textareaElement.getAttribute("maxlength") - textareaElement.value.length
+    
+
+    // Third method  
+    // initalize the max length  
+
+    // textareaElement.maxlength=300
+    // remainingCounterElement.innerText = textareaElement.maxlength - textareaElement.value.length
+
  }
