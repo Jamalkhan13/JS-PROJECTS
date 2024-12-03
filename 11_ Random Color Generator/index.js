9a5ebff284f61c91c6b28613ad99799fff7f62a8
Jamalkhan13 <jamalkhan7858@gmail.com>
2024-12-03 11:59:09 +0500

    Random Color Generator

---
+++ 11_ Random Color Generator/index.js
@@ -0,0 +1,34 @@
+const containerElement = document.querySelector(".container")
+
+for(let index = 0; index<30; index++){
+  const divElement = document.createElement("div")
+  divElement.classList.add("color-container")
+  containerElement.appendChild(divElement)
+}
+
+const divElementColors = document.querySelectorAll(".color-container")
+
+function generateColors (){
+  divElementColors.forEach((divElementColors)=>{
+    const newColor = randomColor();
+    divElementColors.style.backgroundColor = "#" + newColor;  // here divElementColors is from parameter
+    divElementColors.innerText = "#" + newColor
+  })
+}
+
+generateColors()
+
+function randomColor(){
+  const chars = "0123456789abcdef"
+  const colorCodeLength = 6;
+  let colorCode = "";
+  for (let index = 0; index < colorCodeLength; index++) {
+     const randomNumber = Math.floor(Math.random()* chars.length);
+     colorCode += chars.substring(randomNumber, randomNumber+1)
+    
+  }
+  return colorCode;
+}
+
+randomColor()
+
