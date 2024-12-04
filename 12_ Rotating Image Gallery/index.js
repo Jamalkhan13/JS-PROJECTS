f3c281d3c146b215a2890ce53d280278c3eab570
Jamalkhan13 <jamalkhan7858@gmail.com>
2024-12-04 11:53:48 +0500

    Rotating Image Gallery

--- 12_ Rotating Image Gallery/index.js
+++ 12_ Rotating Image Gallery/index.js
@@ -1,1 +1,29 @@
 const imageContainerElement = document.querySelector(".image-container");
+const prevElement = document.getElementById("prev")
+const nextElement = document.getElementById("next")
+
+let x = 0;
+let timer;
+prevElement.addEventListener("click",()=>{
+  x = x + 45;
+  clearTimeout(timer)
+  updatedGallery()
+})
+nextElement.addEventListener("click",()=>{
+  x = x - 45;
+  clearTimeout(timer)
+  updatedGallery()
+})
+
+
+
+ function updatedGallery(){
+  imageContainerElement.style.transform = `perspective(1000px) rotateY(${x}deg)`
+  timer = setTimeout(()=>{
+    x = x - 45;
+    updatedGallery();
+  },3000)
+}
+
+updatedGallery()
+
