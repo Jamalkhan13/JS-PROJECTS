81cde784be9ca4c506398b34857f29455539d0c0
Jamalkhan13 <jamalkhan7858@gmail.com>
2024-12-12 11:30:22 +0500

    Animated Search Bar

--- 13_ Animated Search Bar/index.js
+++ 13_ Animated Search Bar/index.js
@@ -1,3 +1,9 @@
-document.querySelector("search-bar")
+const searchBarContainer = document.querySelector(".search-bar-container")
+const magnifierElement = document.querySelector(".magnifier")
+
+
+magnifierElement.addEventListener("click",()=>{
+  searchBarContainer.classList.toggle("active")
+})
 
 
