e20f9dd6dded97be103de4f9ced5c8b54dbee2da
Jamalkhan13 <jamalkhan7858@gmail.com>
2024-12-17 13:26:34 +0500

    Profile Statistics

--- 18_Profile Statistics/index.js
+++ 18_Profile Statistics/index.js
@@ -1,12 +1,20 @@
 const countersElement = document.querySelectorAll(".counter")
 
-countersElement.forEach(counterElement=>{
-  counterElement.innerText = "0";
+countersElement.forEach(counter=>{
+  counter.innerText = "0";
+  incrementCounter()
+  function incrementCounter(){
+    let currentNumber = +counter.innerText;
+    const dataCeil = +counter.getAttribute("data-ceil")
+    const increment = dataCeil / 15;
 
-  function incrementCounter(counterElement){
-    let currentNumber = +counterElement.innerText;
-    const dataCeil = countersElement.getAtribute("data-Ceil")
-    console.log(dataCeil);
-    
+    currentNumber = Math.ceil(currentNumber + increment)
+    counter.innerText = currentNumber
+    if(currentNumber < dataCeil){
+      counter.innerText = currentNumber;
+      setTimeout(incrementCounter,50)
+    }else{
+      counter.innerText = dataCeil
+    }
   }
 })
