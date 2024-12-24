21d51b07a3c2fd3f4bb640b24e5519f736f05be9
Jamalkhan13 <jamalkhan7858@gmail.com>
2024-12-24 11:06:15 +0500

    Video Trailer Popup

---
+++ 20_ Drum Kits/index.js
@@ -0,0 +1,27 @@
+const kits = ["crash","kick","snare","tom"];
+const containerElement = document.querySelector(".container");
+
+kits.forEach(kit=>{
+  const buttonElement = document.createElement("button");
+  buttonElement.classList.add("btn");
+  buttonElement.innerText = kit;
+  buttonElement.style.backgroundImage = "url(images/" + kit + ".png)"
+  containerElement.appendChild(buttonElement);
+  const audioElement = document.createElement("audio");
+  audioElement.src = "sounds/"+ kit + ".mp3"
+  containerElement.appendChild(audioElement);
+
+  buttonElement.addEventListener("click",()=>{
+    audioElement.play()
+  });
+  window.addEventListener("keydown",(event)=>{
+    if(event.key === kit.slice(0,1)){
+      audioElement.play();
+      buttonElement.style.transform = "scale(.9)";
+      setTimeout(()=>{
+        buttonElement.style.transform = "scale(1)"
+      },100)
+    }
+    
+  })
+})
